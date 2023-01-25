66c40fdcd2405d0ed0500832ddf39c92be58ea93
dondavinayreddy@gmail.com <39482416+VinayR1@users.noreply.github.com>
2023-01-24 19:39:27 -0600

    Create Accessingofinstancevariableinsidemain.java

---
+++ Accessingofinstancevariableinsidemain.java
@@ -0,0 +1,9 @@
+class Instancevariable
+{
+    int a = 100;
+    public static void main (String[] args)
+    {
+      Instancevariable ref = new Instancevariable();
+      System.out.println(ref.a);
+    }
+}
