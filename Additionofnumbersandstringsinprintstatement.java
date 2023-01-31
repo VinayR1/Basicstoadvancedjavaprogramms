93b464dc393c16052fb3ac9524fc18207fce3eb0
dondavinayreddy@gmail.com <39482416+VinayR1@users.noreply.github.com>
2023-01-31 16:30:38 -0600

    Create Additionofnumbersandstringsinprintstatement.java

---
+++ Additionofnumbersandstringsinprintstatement.java
@@ -0,0 +1,14 @@
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class Additionofnumbersandstringsinprintstatement {
+	public static void main (String[] args) 
+	
+	{
+		System.out.println(20+31.f);
+    System.out.println(20+31.5f+ "hi" + "Vinay");
+	}
+}
+
+// output 51.5hiVinay
